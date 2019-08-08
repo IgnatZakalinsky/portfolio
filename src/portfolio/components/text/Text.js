cc6e29ab1496168e8d55ccad2c51ed3b413230ec
Ignat <ai73a@yandex.ru>
2019-08-08 18:01:41 +0300

    phone button

--- src/portfolio/components/text/Text.js
+++ src/portfolio/components/text/Text.js
@@ -4,10 +4,14 @@ import Title from "../../common/title/Title";
 import NekoButton from "../../common/nekoButton/NekoButton";
 
 const Text = () => {
+    const phone = () => alert('Life :) +375 25 633 21 46');
+
     return <div className={s.text}>
         <Title t={'I will consider options for remote work'}/>
         <div className={s.line}></div>
-        <NekoButton t={'HIRE ME'}/>
+        <div onClick={phone}>
+            <NekoButton t={'HIRE ME'}/>
+        </div>
     </div>
 };
 

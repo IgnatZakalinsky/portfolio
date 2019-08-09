a6d1acf6e89b8fa26e16635f757b391a2d3d08f8
Ignat <ai73a@yandex.ru>
2019-08-09 15:52:14 +0300

    add email

--- src/portfolio/common/nekoButton/NekoButton.js
+++ src/portfolio/common/nekoButton/NekoButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './NekoButton.module.css'
 
 const NekoButton = (props) => {
-    return <button className={s.b}>
+    return <button className={s.b} onClick={props.onClick}>
         {props.t}
     </button>
 };
--- src/portfolio/components/contacts/Contacts.js
+++ src/portfolio/components/contacts/Contacts.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import s from './Contacts.module.css'
 import Title from "../../common/title/Title";
-import NekoButton from "../../common/nekoButton/NekoButton";
+//import NekoButton from "../../common/nekoButton/NekoButton";
 
 const Contacts = () => {
     return <div className={s.contacts}>
         <a name="Contact" className={s.a}/>
         <div className={s.contContainer}>
             <Title t={'Contact'}/>
-            <div className={s.line}></div>
+            <div className={s.line}/>
             <div className={s.inputs}>
-                <input placeholder={'Name'} className={s.bc}/>
+                ai73a@yandex.ru
+                {/*<input placeholder={'Name'} className={s.bc}/>
                 <input placeholder={'Email'} className={s.bc}/>
-                <textarea rows={7} className={s.bc}/>
+                <textarea rows={7} className={s.bc}/>*/}
             </div>
-            <NekoButton t={'SEND MESSAGE'}/>
+            {/*<NekoButton t={'SEND MESSAGE'}/>*/}
         </div>
     </div>
 };

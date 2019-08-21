fec503b26e6944d4b16a38caa5f0d4d48604c38f
Ignat <ai73a@yandex.ru>
2019-08-21 17:05:12 +0300

    Updates

---
+++ precache-manifest.a1a56f1a684eadba261ef905d8b2acb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf4bc31905b169a55cbe3f816019081",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ee43eaa0178c257eba10",
+    "url": "/portfolio/static/css/main.eceeb8dc.chunk.css"
+  },
+  {
+    "revision": "8f7d47faccb539a7375a",
+    "url": "/portfolio/static/js/2.b62ef667.chunk.js"
+  },
+  {
+    "revision": "649a0a76182c8a919676",
+    "url": "/portfolio/static/js/3.6be9a0c3.chunk.js"
+  },
+  {
+    "revision": "2e5a57ee79edbfecedff",
+    "url": "/portfolio/static/js/4.ea3710c4.chunk.js"
+  },
+  {
+    "revision": "ee43eaa0178c257eba10",
+    "url": "/portfolio/static/js/main.4ace817e.chunk.js"
+  },
+  {
+    "revision": "d0dd9d1badababa65171",
+    "url": "/portfolio/static/js/runtime~main.87e89ed1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0562f5a3ae69e3dd582373dac248fb8e.js"
+  "/portfolio/precache-manifest.a1a56f1a684eadba261ef905d8b2acb3.js"
 );
 
 self.addEventListener('message', (event) => {
